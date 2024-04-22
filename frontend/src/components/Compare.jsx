e9c25feca23e6d687f27fb7fd1fbbea98130e106
michellechang02 <changmi@seas.upenn.edu>
2024-04-22 14:01:47 -0400

    navbar routes

---
+++ frontend/src/components/Compare.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Compare(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Compare;
---
+++ frontend/src/components/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Products(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Products;
