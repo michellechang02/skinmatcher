c6797b27267258698bab5ebf9715a7c720012aa2
michellechang02 <changmi@seas.upenn.edu>
2024-04-22 15:12:14 -0400

    interactive comparisons

--- frontend/src/components/Compare.jsx
+++ frontend/src/components/Compare.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { ChakraProvider, Box, Flex, Text, Image, Input, HStack, VStack, Button,
     FormControl, FormLabel} from '@chakra-ui/react';
 import home from '../home.json'
 
 function Compare(props) {
+
+    const [input1, setInput1] = useState('');
+    const [input2, setInput2] = useState('');
+    const [input3, setInput3] = useState('');
+
+
+
+    function handleInput1() {
+        console.log(input1);
+        setInput1('');
+    }
+
+    function handleInput2() {
+        console.log(input2);
+        setInput2('');
+    }
+
+    function handleInput3() {
+        console.log(input3); 
+        setInput3('');
+    }
+
+    useEffect(() => {
+
+    }, [])
+
+
+
+
+
+
     return (
         <div>
-            <Text ml={5} fontSize="5xl" mt={5}>
+        <Text ml={5} fontSize="5xl" mt={5}>
         Compare
       </Text>
       <Flex justifyContent="space-around" alignItems="center">
@@ -14,8 +45,8 @@ function Compare(props) {
           <VStack spacing={2} align="stretch">
             <Text>Product 1:</Text>
             <HStack>
-              <Input placeholder="Enter details for Product 1" />
-              <Button colorScheme="blackAlpha">Add</Button>
+              <Input placeholder="Enter details for Product 1" value={input1} onChange={(e) => setInput1(e.target.value)}/>
+              <Button colorScheme="blackAlpha" onClick={handleInput1}>Add</Button>
             </HStack>
           </VStack>
         </Box>
@@ -23,8 +54,8 @@ function Compare(props) {
           <VStack spacing={2} align="stretch">
             <Text>Product 2:</Text>
             <HStack>
-              <Input placeholder="Enter details for Product 2" />
-              <Button colorScheme="blackAlpha">Add</Button>
+              <Input placeholder="Enter details for Product 2" value={input2} onChange={(e) => setInput2(e.target.value)}/>
+              <Button colorScheme="blackAlpha" onClick={handleInput2}>Add</Button>
             </HStack>
           </VStack>
         </Box>
@@ -32,15 +63,17 @@ function Compare(props) {
           <VStack spacing={2} align="stretch">
             <Text>Product 3:</Text>
             <HStack>
-              <Input placeholder="Enter details for Product 3" />
-              <Button colorScheme="blackAlpha">Add</Button>
+              <Input placeholder="Enter details for Product 3" value={input3} onChange={(e) => setInput3(e.target.value)}/>
+              <Button colorScheme="blackAlpha" onClick={handleInput3}>Add</Button>
             </HStack>
           </VStack>
         </Box>
       </Flex>
             <Flex justifyContent="space-around" alignItems="center">
+                
         <Box  w="400px" h="550px"  bg="#F5F5F5" boxShadow="md" mt={2}>
-            <Text textAlign="center" mt={2} fontSize="40px">Product 1</Text>
+        {input1 !== '' &&  <div>
+            <Text textAlign="center" mt={2} fontSize="40px">{input1}</Text>
             
             <Image
               src={home[0].url}
@@ -51,11 +84,24 @@ function Compare(props) {
             />
             <Text textAlign="center" mt={2}>Price: $2.00</Text>
             <Text ml={5}>Based on your:</Text>
+            <HStack width="80%" justifyContent="space-between">
             <Text ml={5}>Desired Product</Text>
+            <Text ml={5}>70%</Text>
+            </HStack>
+            <HStack width="80%" justifyContent="space-between">
             <Text ml={5}>Acne Control</Text>
+            <Text ml={5}>60%</Text>
+            </HStack>
+            <HStack width="80%" justifyContent="space-between">
             <Text ml={5}>Face Washing Routine</Text>
+            <Text ml={5}>50%</Text>
+            </HStack></div>
+         }
         </Box>
+        
+        
         <Box w="400px" h="550px" bg="#F5F5F5" boxShadow="md" mt={2}>
+        {input2 !== '' &&  <div>
         <Text textAlign="center" mt={2} fontSize="40px">Product 2</Text>
         
             <Image
@@ -67,11 +113,25 @@ function Compare(props) {
             />
         <Text textAlign="center" mt={2}>Price: $1.00</Text>
         <Text ml={5}>Based on your:</Text>
+        <HStack width="80%" justifyContent="space-between">
         <Text ml={5}>Desired Product</Text>
-            <Text ml={5}>Acne Control</Text>
-            <Text ml={5}>Face Washing Routine</Text>
+        <Text ml={5}>70%</Text>
+        </HStack>
+        <HStack width="80%" justifyContent="space-between">
+        <Text ml={5}>Acne Control</Text>
+        <Text ml={5}>60%</Text>
+        </HStack>
+        <HStack width="80%" justifyContent="space-between">
+        <Text ml={5}>Face Washing Routine</Text>
+        <Text ml={5}>50%</Text>
+        </HStack></div>
+         }
+         
         </Box>
+       
+        
         <Box w="400px" h="550px" bg="#F5F5F5" boxShadow="md" mt={2}>
+        {input3 !== '' && <div>
         <Text textAlign="center" mt={2} fontSize="40px">Product 3</Text>
         
             <Image
@@ -83,10 +143,21 @@ function Compare(props) {
             />
         <Text textAlign="center" mt={2}>Price: $2.00</Text>
         <Text ml={5}>Based on your:</Text>
+        <HStack width="80%" justifyContent="space-between">
         <Text ml={5}>Desired Product</Text>
+        <Text ml={5}>70%</Text>
+        </HStack>
+        <HStack width="80%" justifyContent="space-between">
         <Text ml={5}>Acne Control</Text>
+        <Text ml={5}>60%</Text>
+        </HStack>
+        <HStack width="80%" justifyContent="space-between">
         <Text ml={5}>Face Washing Routine</Text>
+        <Text ml={5}>50%</Text>
+        </HStack></div>
+        }
         </Box>
+       
       </Flex>
         </div>
     );
--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,8 +1,61 @@
 import React from 'react';
+import {Text,
+    Input,
+    Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Box
+} from '@chakra-ui/react';
 
 function Products(props) {
+
+    const products = [
+        { id: 1, name: "1025 Dokdo Cleanser", price: "$13.90", category: "Cleanser" },
+        { id: 2, name: "mixsoon Bean Essence", price: "$29.75", category: "Essence" },
+        { id: 3, name: "BoJ Glow Serum", price: "$30.43", category: "Serum" },
+        { id: 4, name: "AM Facial Moisturizing Lotion SPF 50", price: "$30.40", category: "Sunscreen" },
+        { id: 5, name: "Logitech MX Master 3 Mouse", price: "$99.99", category: "Accessories" }
+        // Add more products as needed
+      ];
+
+
+
+
+
     return (
         <div>
+        <Text ml={5} fontSize="5xl" mt={5}>
+        Products
+      </Text>
+      
+      <Box overflowX="auto">
+        <TableContainer>
+          <Table colorScheme="blackAlpha">
+            <Thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Name</Th>
+                <Th>Price</Th>
+                <Th>Category</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {products.map((product) => (
+                <Tr key={product.id}>
+                  <Td>{product.id}</Td>
+                  <Td>{product.name}</Td>
+                  <Td>{product.price}</Td>
+                  <Td>{product.category}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
             
         </div>
     );
