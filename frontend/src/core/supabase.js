cff153a397c9db6765b99db94fc93f810ecd8db2
michellechang02 <changmi@seas.upenn.edu>
2024-04-24 14:13:24 -0400

    deploy

--- frontend/src/core/supabase.js
+++ frontend/src/core/supabase.js
@@ -2,7 +2,7 @@ import {createClient} from "@supabase/supabase-js"
 
 export const supabase = createClient(
     process.env.REACT_APP_SUPABASE_URL,
-    process.env.REACT_APP_SUPABASE_URL,
+    process.env.REACT_APP_SUPABASE_KEY,
     {
         auth: {
             autoRefreshToken: true,
