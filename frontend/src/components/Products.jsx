e5a9e9b089f79aa1e299e57d2141972941741481
michellechang02 <changmi@seas.upenn.edu>
2024-04-24 09:03:07 -0400

    products page

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer, Text
+  } from '@chakra-ui/react'
 
 function Products(props) {
     return (
         <div>
+        <Text ml={5} fontSize="5xl" mt={5}>
+        Products
+        </Text>
+        <TableContainer>
+        <Table variant='simple'>
+            <Thead>
+            <Tr>
+                <Th>Product</Th>
+                <Th>Price</Th>
+                <Th>Recommendation</Th>
+            </Tr>
+            </Thead>
+            <Tbody>
+            <Tr>
+                <Td>Product1</Td>
+                <Td>$29.00</Td>
+                <Td>70.5%</Td>
+            </Tr>
+            <Tr>
+                <Td>Product2</Td>
+                <Td>$12.00</Td>
+                <Td>30.4%</Td>
+            </Tr>
+            </Tbody>
+        </Table>
+        </TableContainer>
             
         </div>
     );
